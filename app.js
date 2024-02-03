d82edeb19410abd3802e8470854fa94b972eda75
Egorka.com <74678024+Egorka-com@users.noreply.github.com>
2024-02-03 16:39:31 +0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,38 @@
+let app = window.Telegram.WebApp;
+
+app.expand();
+
+app.MainButton.textColor = "#FFFFFF";
+app.MainButton.color = "#2cab37";
+
+let item = "данные";
+
+let btn1 = document.getElementById("btn1");
+
+btn1.addEventListener("click", OnClick);
+function OnClick()
+{
+    if(app.MainButton.isVisible)
+    {
+        app.MainButton.hide();
+    }
+    else
+    {
+        app.MainButton.setText("вы нажали на кнопку");
+        app.MainButton.show();
+    }
+}
+
+Telegram.WebApp.onEvent("mainButtonClicked", OnInit);
+function OnInit()
+{
+    Telegram.sendData(item);
+}
+
+let userInfo = document.getElementById("userInfo");
+
+let p = document.createElement("p");
+p.innerText = `${app.initDataUnsafe.first_name}`
+p.innerText = `${app.initDataUnsafe.last_name}`
+
+userInfo.appendChild(p);
